1d091dde02e741b2a098e291f8136943000de930
lebrancconvas <lebrancconvas@gmail.com>
2022-08-10 01:05:32 +0700

    ✨ feat: update homepage

---
+++ pages/data/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Data: NextPage = () => {
+	return(
+		<div>
+			<Head>
+				<title>Data</title>
+			</Head>
+			<div>
+				<h1>Data</h1>
+			</div>
+		</div>
+	)
+};
+
+export default Data;
