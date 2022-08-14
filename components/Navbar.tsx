4d7e709ebced82153dafba255d35904d1c1fbd5b
lebrancconvas <lebrancconvas@gmail.com>
2022-08-14 12:24:50 +0700

    💄 style: decorate navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,13 @@
 import type { NextComponentType } from 'next';
+import { Box, useColorModeValue } from '@chakra-ui/react';
 
 const Navbar: NextComponentType = () => {
+	const bgNavbarColor = useColorModeValue('green.300', 'green.500');
 	return(
 		<div>
-
+			<Box bg={bgNavbarColor} w="100%" p={4} color="white">
+				Poom Yimyuean
+			</Box>
 		</div>
 	)
 };
